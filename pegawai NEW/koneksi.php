525e2de50204bf25b85895d87e774c583fe5cce0
Anggi periawan <80029982+anggiperiawan@users.noreply.github.com>
2022-07-03 09:31:30 +0700

    Add files via upload

---
+++ pegawai NEW/koneksi.php
@@ -0,0 +1,15 @@
+<?php
+$conn_str = "host=john.db.elephantsql.com " .
+    "port=5432 " .
+    "user=iaijbunw " .
+    "dbname=iaijbunw " .
+    "password=11ETSyt41QMmqzYr4R9y1eF3TdRow3sw";
+$conn = pg_connect($conn_str);
+
+//cek koneksi
+/* if($conn) {
+    header('Location: ./frontend');
+} else {
+    echo "<h3>Koneksi GAGAL TERSAMBUNG</h3>";
+} */
+?>
